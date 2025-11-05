4e8d6342ae557a295e47126a08f534b0406eb33e
Erwin Wildenburg <erwin.wildenburg@transavia.com>
2025-11-05 13:47:51 +0100

    Initial

---
+++ Dockerfile
@@ -0,0 +1,20 @@
+FROM alpine:latest
+
+RUN apk add --update bash \
+                     python3 \
+                     bash \
+                     xz \
+                     alpine-sdk \
+                     cifs-utils \
+                     libc6-compat \
+                     jq \
+                     perl
+WORKDIR /root
+COPY entrypoint.sh .
+COPY config_modification.json .
+
+ENV PLATFORM="GeminiLake"
+ENV DSM_VERSION="7.2-72806"
+ENV KERNEL_VERSION="4.4.302"
+
+ENTRYPOINT ["/root/entrypoint.sh"]
---
+++ entrypoint.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+
+L_PLATFORM=$(echo $PLATFORM | tr '[:upper:]' '[:lower:]')
+KERNEL_MINOR_VERSION=$(echo $KERNEL_VERSION | cut -d. -f1,2)
+
+# Setup Synology package toolkit framework
+git clone -b DSM7.2 https://github.com/SynologyOpenSource/pkgscripts-ng /toolkit/pkgscripts-ng
+/toolkit/pkgscripts-ng/EnvDeploy -v $(echo $DSM_VERSION | cut -d- -f1) -p $L_PLATFORM
+
+# Setup Synology toolchain
+wget https://global.synologydownload.com/download/ToolChain/toolchain/$DSM_VERSION/Intel%20x86%20Linux%20$KERNEL_VERSION%20%28$PLATFORM%29/$L_PLATFORM-gcc1220_glibc236_x86_64-GPL.txz
+TOOLCHAIN_FOLDER=$(tar -tJf "$L_PLATFORM-gcc1220_glibc236_x86_64-GPL.txz" | head -n 1 | cut -d/ -f1)
+tar -xJf "$L_PLATFORM-gcc1220_glibc236_x86_64-GPL.txz" -C /usr/local/
+
+# Setup Synology kernel
+wget https://global.synologydownload.com/download/ToolChain/Synology%20NAS%20GPL%20Source/$DSM_VERSION/$L_PLATFORM/linux-$KERNEL_MINOR_VERSION.x.txz
+KERNEL_FOLDER=$(tar -tJf linux-$KERNEL_MINOR_VERSION.x.txz | head -n 1 | cut -d/ -f1)
+tar -xJf linux-$KERNEL_MINOR_VERSION.x.txz -C /usr/local/$TOOLCHAIN_FOLDER/
+cp /usr/local/$TOOLCHAIN_FOLDER/$KERNEL_FOLDER/synoconfigs/$L_PLATFORM /usr/local/$TOOLCHAIN_FOLDER/$KERNEL_FOLDER/.config
+CONFIG_CROSS_COMPILE=$(printf '%s\n' "/usr/local/$TOOLCHAIN_FOLDER/bin/$TOOLCHAIN_FOLDER-" | sed 's/[\/&]/\\&/g')
+sed -i "s/CONFIG_CROSS_COMPILE=\"\"/CONFIG_CROSS_COMPILE=\"$CONFIG_CROSS_COMPILE\"/" /usr/local/$TOOLCHAIN_FOLDER/$KERNEL_FOLDER/.config
+
+# Setup WireGuard
+git clone https://git.zx2c4.com/wireguard-linux-compat
+sed -i "s|KERNELDIR ?= .*|KERNELDIR ?= /usr/local/$TOOLCHAIN_FOLDER/$KERNEL_FOLDER|" wireguard-linux-compat/src/Makefile
+
+# Override kernel configs
+jq -r 'to_entries[] | "\(.key) \(.value)"' config_modification.json | while read -r key value; do
+  sed -i "s|[# ].*$key.*|$value|" /usr/local/$TOOLCHAIN_FOLDER/$KERNEL_FOLDER/.config
+
+  if ! grep -q "^$key=" /usr/local/$TOOLCHAIN_FOLDER/$KERNEL_FOLDER/.config; then
+    echo "$value" >> /usr/local/$TOOLCHAIN_FOLDER/$KERNEL_FOLDER/.config
+  fi
+done
+
+# Build
+make -C /usr/local/$TOOLCHAIN_FOLDER/$KERNEL_FOLDER oldconfig
+make -C /usr/local/$TOOLCHAIN_FOLDER/$KERNEL_FOLDER prepare
+make -C /usr/local/$TOOLCHAIN_FOLDER/$KERNEL_FOLDER modules_prepare
+make -C /usr/local/$TOOLCHAIN_FOLDER/$KERNEL_FOLDER modules -j$(nproc) KBUILD_MODPOST_NOFINAL=1
+
+# Build the modules we need
+make -C /usr/local/$TOOLCHAIN_FOLDER/$KERNEL_FOLDER -j$(nproc) M=net/ipv4/netfilter modules
+make -C /usr/local/$TOOLCHAIN_FOLDER/$KERNEL_FOLDER -j$(nproc) M=net/ipv6/netfilter modules
+make -C /usr/local/$TOOLCHAIN_FOLDER/$KERNEL_FOLDER -j$(nproc) M=net/netfilter modules
+make -C /usr/local/$TOOLCHAIN_FOLDER/$KERNEL_FOLDER -j$(nproc) M=drivers/usb/serial modules
+make -C wireguard-linux-compat/src -j$(nproc)
+
+# Collect output
+OUTPUT_FOLDER="./output/${PLATFORM}_${DSM_VERSION}_${KERNEL_VERSION}"
+mkdir -p $OUTPUT_FOLDER
+cp /usr/local/$TOOLCHAIN_FOLDER/$KERNEL_FOLDER/net/ipv4/netfilter/iptable_raw.ko $OUTPUT_FOLDER
+cp /usr/local/$TOOLCHAIN_FOLDER/$KERNEL_FOLDER/net/ipv6/netfilter/ip6table_raw.ko $OUTPUT_FOLDER
+cp /usr/local/$TOOLCHAIN_FOLDER/$KERNEL_FOLDER/net/netfilter/xt_comment.ko $OUTPUT_FOLDER
+cp /usr/local/$TOOLCHAIN_FOLDER/$KERNEL_FOLDER/net/netfilter/xt_connmark.ko $OUTPUT_FOLDER
+cp /usr/local/$TOOLCHAIN_FOLDER/$KERNEL_FOLDER/drivers/usb/serial/cp210x.ko $OUTPUT_FOLDER
+cp /usr/local/$TOOLCHAIN_FOLDER/$KERNEL_FOLDER/drivers/usb/serial/ch341.ko $OUTPUT_FOLDER
+cp /usr/local/$TOOLCHAIN_FOLDER/$KERNEL_FOLDER/drivers/usb/serial/pl2303.ko $OUTPUT_FOLDER
+cp /usr/local/$TOOLCHAIN_FOLDER/$KERNEL_FOLDER/drivers/usb/serial/ti_usb_3410_5052.ko $OUTPUT_FOLDER
+cp wireguard-linux-compat/src/wireguard.ko $OUTPUT_FOLDER
